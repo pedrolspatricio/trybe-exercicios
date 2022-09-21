b2a2610daf6c94bd790f213e00263f83604efbf2
Pedro <pedrolucas.contato@gmail.com>
2022-09-21 20:02:07 -0300

    conclusão exercício Prática

---
+++ fundamentos/secoes-04-introducao-a-HTML-&-CSS/dia-01-javaScript-primeiros-passos/para-fixar-01/scrips6.js
@@ -0,0 +1,55 @@
+// const a = 10
+// const b = 5
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+// if (a>b) {
+//     console.log('a é maior que b')
+// } else{
+//     console.log('b é maior que a')
+// }
+
+// const c = 10
+// const d = 5
+// const e = 15
+
+// if (c>d && c>e ) {
+//     console.log('C é o maior número')
+// } else if (d>c && d>e) {
+//     console.log('D é o maior número')
+// } else {
+//     console.log('E é o maior número')
+// }
+
+// const f = 0
+
+// if (f>0) {
+//     console.log('positive')
+// } else if (f<0) {
+//     console.log('negative')
+// } else {
+//     console.log('zero')
+// }
+
+// const angle1 = 90
+// const angle2 = 30
+// const angle3 = 60
+
+// if (angle1<0 || angle2<0 || angle3<0) {
+//     console.log('Valor inválido')
+// } else if (angle1 + angle2 + angle3 == 180) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// const pecaXadrez
+
+// switch(pecaXadrez){
+
+// }
+
