a34d62106464b3e252dd236618bc720cd827e7f9
Pedro <pedrolucas.contato@gmail.com>
2022-09-22 18:11:29 -0300

    Iniciando os exercícios

---
+++ fundamentos/secoes-04-introducao-a-HTML-&-CSS/dia-02-javaScript-array-e-loop-for/exercicio1.js
@@ -0,0 +1,41 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// console.log(menu);
+
+// menu.push('Contato')
+
+// console.log(menu)
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index++) {
+//     const element = groceryList[index];
+//     console.log(element);
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+// for (let index = 0; index < names.length; index++) {
+//     const nameList = names[index];
+//     console.log(nameList)
+// }
+
+// for (let index of names){
+//     console.log(index)
+// }
+
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
