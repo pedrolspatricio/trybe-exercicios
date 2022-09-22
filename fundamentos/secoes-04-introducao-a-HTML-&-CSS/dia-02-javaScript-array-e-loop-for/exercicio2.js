c1874082eb058a58627aeef67452b7b29f71775d
Pedro <pedrolucas.contato@gmail.com>
2022-09-22 19:51:41 -0300

    Todos os programas

--- fundamentos/secoes-04-introducao-a-HTML-&-CSS/dia-02-javaScript-array-e-loop-for/exercicio2.js
+++ fundamentos/secoes-04-introducao-a-HTML-&-CSS/dia-02-javaScript-array-e-loop-for/exercicio2.js
@@ -23,6 +23,56 @@ if(sum > 20) {
     console.log('valor menor ou igual a 20')
 };
 
+let maxValue = 0;
 
+for (index = 0; index < numbers.length; index += 1){
+    if (numbers[index] > maxValue) {
+        maxValue = numbers[index];
+    } else {
+        maxValue;
+    }
+}
 
+console.log(maxValue)
 
+let oddNumbers = 0;
+
+for (index = 0; index < numbers.length; index += 1){
+    if (numbers[index] % 2 != 0) {
+        oddNumbers += 1;
+    } else {
+        oddNumbers;
+    }
+}
+
+if (oddNumbers === 0){
+    console.log('nenhum valor encontrado')
+} else {
+    console.log(oddNumbers)
+}
+
+let lowerNumber = numbers[0];
+
+for (index = 1; index < numbers.length; index += 1){
+    if (numbers[index] < lowerNumber) {
+        lowerNumber = numbers[index];
+    }
+}
+
+console.log(lowerNumber)
+
+let numbers2 = [];
+
+for (index = 1; index <= 25; index += 1){
+    numbers2.push(index);
+}
+
+console.log(numbers2)
+
+let divisionNumbers = [];
+
+for(let index = 0; index < numbers2.length; index += 1) {
+    divisionNumbers.push(numbers2[index] / 2) 
+}
+
+console.log(divisionNumbers)
