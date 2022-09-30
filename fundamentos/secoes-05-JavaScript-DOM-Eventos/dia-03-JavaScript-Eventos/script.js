119e170ce03a124001228b23222feb5261515aff
Pedro <pedrolucas.contato@gmail.com>
2022-09-30 19:04:51 -0300

    parte 1

--- fundamentos/secoes-05-JavaScript-DOM-Eventos/dia-03-JavaScript-Eventos/script.js
+++ fundamentos/secoes-05-JavaScript-DOM-Eventos/dia-03-JavaScript-Eventos/script.js
@@ -1,7 +1,7 @@
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
-  
+    
     for (let index = 0; index < weekDays.length; index += 1) {
       const days = weekDays[index];
       const dayListItem = document.createElement('li');
@@ -13,5 +13,34 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  function createCalendar() {
+    
+    const mounthDays = document.getElementById('days');
+
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+    const day = decemberDaysList[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = day;
+    
+    mounthDays.appendChild(dayListItem);
+    dayListItem.className = 'day';
+    
+
+    if (day === 24 || day === 31) {
+        dayListItem.className = 'day holiday';
+    } else if (day === 4 || day === 11 || day === 18) {
+        dayListItem.className = 'day friday';
+    } else if (day ===25) {
+        dayListItem.className = 'day holiday friday';
+    } else {
+        dayListItem.className = 'day';
+    }
+    }
+    }
+
+  createCalendar();
+  
+ 
   
